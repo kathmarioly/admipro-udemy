825de11d8e9f663221066a569fca5892b4743837
kathmarioly <katerinemarioly@gmail.com>
2019-11-09 17:11:39 -0300

    Módulos agregados y modificados

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+// Componentes:
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+// Modulos:
+import { SharedModule } from '../shared/shared.module';
+// Rutas:
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+   declarations : [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+   ],
+   exports : [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+   ],
+   imports : [
+    SharedModule,
+    PAGES_ROUTES
+     ],
+})
+
+export class PagesModule { }
